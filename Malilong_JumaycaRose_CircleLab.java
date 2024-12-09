db297b1384b3237ed3011393ca9eae64d98f449b
Jumayca Malilong <malilongjumayca@g.cjc.edu.ph>
2024-12-09 23:57:35 +0900

    Update Malilong_JumaycaRose_CircleLab.java

--- Malilong_JumaycaRose_CircleLab.java
+++ Malilong_JumaycaRose_CircleLab.java
@@ -1,64 +1,31 @@
-
 package malilong_jumayca.rose_circlelab;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Malilong_JumaycaRose_CircleLab {
 
-    
     public static void main(String[] args) {
-      
-        boolean loop = true;
 
-        while (loop == true) {
-            Circle cir = new Circle();
+        // Prompt the user to input a radius
+        try (Scanner scanner = new Scanner(System.in)) {
 
-            String response;
-            String response2;
-            double radius = 0.0;
-            double angle = 0;
-            int convert = 0;
-            int convert2 = 0;
+            // Prompt the user to input a radius
+            System.out.println("Please enter the radius of the circle: ");
+            double userRadius = scanner.nextDouble();
+            System.out.println();
 
-            // TODO code application logic here
-            int opt = JOptionPane.showConfirmDialog(null, "Welcome to circle calculator", "Click YES to continue", JOptionPane.YES_NO_OPTION);
-            if (opt == JOptionPane.YES_OPTION) {
-                String code = JOptionPane.showInputDialog("Select a specific method"
-                        + "\n Calculation                           \t code"
-                        + "\n Area of Circle                          \t 1 "
-                        + "\n Circumference of Circle       \t 2 "
-                        + "\n Diameter of Circle                  \t 3 ");
-                switch (code) {
-                    case "1":
-                        response = JOptionPane.showInputDialog("Radius:");
-                        convert = Integer.parseInt(response);
-                        JOptionPane.showMessageDialog(null, "Area of Circle=" + cir.area(convert));
-                        break;
-                    case "2":
-                        response = JOptionPane.showInputDialog("Radius:");
-                        convert = Integer.parseInt(response);
-                        JOptionPane.showMessageDialog(null, "Circumference of Circle = " + cir.circumfer(convert));
-                        break;
-                    case "3":
-                        response = JOptionPane.showInputDialog("Radius:");
-                        convert = Integer.parseInt(response);
-                        JOptionPane.showMessageDialog(null, "Diameter of Circle = " + cir.diameter(convert));
-                        break;
+            // Circle object with user-provided input
+            Circle circle = new Circle(userRadius);
 
-                    default:
-                        JOptionPane.showMessageDialog(null, " ERROR!!!!!!!!!! ");
-                }
-
-            }
-                int response3 = JOptionPane.showConfirmDialog(null, "Do you want to perform another calculation?", "Continue?", JOptionPane.YES_NO_OPTION);
-
-                if (response3 == JOptionPane.NO_OPTION) {
-                    loop = false;
-                }
-            }
+            // Display the circle's details
+            System.out.println("Welcome to Circle Calculator");
+            System.out.println("Radius: " + circle.getRadius());
+            System.out.println("Area: " + circle.getArea());
+            System.out.println("Circumference: " + circle.getCircumference());
+            System.out.println("Diameter: " + circle.getDiameter());
 
         }
 
+        System.out.println("Thank you for using the Circle Calculator!");
     }
-
-    
+}
